fc687fbe2caa4a9817052e61287d94d891851792
NatsukiShimizu <n.shimizu023@gmail.com>
2024-02-28 15:10:27 +0900

    サンプル画面作成

---
+++ app/sample.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/login')
+def login():
+    html = render_template('sample.html')
+    return html
+
+if __name__ == "__main__":
+    app.run(port=50000, debug=True)

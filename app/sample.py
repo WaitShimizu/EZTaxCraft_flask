c69c26432b462854bfdca6d766b9bca26d838040
NatsukiShimizu <n.shimizu023@gmail.com>
2024-03-02 17:51:07 +0900

    htmlを使用して文字にフリガナを振る

--- app/sample.py
+++ app/sample.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/login')
 def login():
-    html = render_template('sample.html')
+    html = render_template('sample_n.html')
     return html
 
 if __name__ == "__main__":

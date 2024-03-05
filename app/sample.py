b4bee07b64a6744c779257a0c48687023705de46
NatsukiShimizu <n.shimizu023@gmail.com>
2024-03-05 10:31:05 +0900

    簡易登録フォーム作成

--- app/sample.py
+++ app/sample.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/login')
 def login():
-    html = render_template('sample_n.html')
+    # html = render_template('sample_n.html')
+    html = render_template('study.html')
     return html
 
 if __name__ == "__main__":

d3a6d25b015389ea1fe35d75da4a4212d3e3a258
NatsukiShimizu <n.shimizu023@gmail.com>
2024-03-15 11:03:30 +0900

    ファイル作成、処理記入(top.html)

--- app/sample.py
+++ app/sample.py
@@ -1,14 +1,20 @@
-from flask import Flask
 from flask_login import LoginManager
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    # html = render_template('sample_n.html')
-    html = render_template('study.html')
+    if request.method == 'POST':
+        # POSTリクエストの場合、認証情報を取得して認証を行う
+        email = request.form.get('email')
+        password = request.form.get('password')
+        # ここでユーザーの認証を行う処理を追加する
+        # ログインに成功したら適切なページにリダイレクトするなどの処理を行う
+        return f'Login Successful! email:{email} password:{password}'
+
+    html = render_template('top.html')
     return html
 
 if __name__ == "__main__":

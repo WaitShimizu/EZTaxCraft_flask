09effe89a81ab565b6c7e7b0d205004263390c33
NatsukiShimizu <n.shimizu023@gmail.com>
2024-03-18 11:19:14 +0900

    waitロゴにリンク追加

--- app/sample.py
+++ app/sample.py
@@ -4,18 +4,26 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        # POSTリクエストの場合、認証情報を取得して認証を行う
-        email = request.form.get('email')
-        password = request.form.get('password')
-        # ここでユーザーの認証を行う処理を追加する
-        # ログインに成功したら適切なページにリダイレクトするなどの処理を行う
-        return f'Login Successful! email:{email} password:{password}'
-
+@app.route('/templates', methods=['GET'])
+def top():
     html = render_template('top.html')
     return html
+
+@app.route('/top', methods=['GET'])
+def back_top():
+    html = render_template('top.html')
+    return html
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    html = render_template('login.html')
+    return html
+
+@app.route('/user_regist', methods=['GET', 'POST'])
+def user_regist():
+    html = render_template('user_regist.html')
+    return html
+
 
 if __name__ == "__main__":
     app.run(port=50000, debug=True)

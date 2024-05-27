a33a3ff97d504ad12aa3cafc1abe8d452e8fa061
NatsukiShimizu <n.shimizu023@gmail.com>
2024-05-27 17:32:44 +0900

    クラス名、id名変更、位置調整

--- app/static/js/password_icon.js
+++ app/static/js/password_icon.js
@@ -20,3 +20,18 @@ function onClickView() {
         console.log("Clicked! 2");
     }
 };
+
+
+// チェックボックスイベント処理
+let element = document.getElementById('agreement');
+element.addEventListener('change', function(click_element){
+
+    //ここにイベントの内容を記述
+    if (element.checked == true){
+        //利用規約に同意していた場合に処理する内容を記述する
+
+    } else {
+        //利用規約に同意しない場合に処理する内容を記述する
+        // ※何も処理しない(ボタンを非活性に設定する必要を考慮する)
+    }
+});

1b3d4bd4de4ba103ac5d10c08376b92b79bee175
NatsukiShimizu <n.shimizu023@gmail.com>
2024-06-07 17:57:52 +0900

    パスワードのバリデーション機能の関数を共通化修正

--- app/static/js/regist_btn_ctrl.js
+++ app/static/js/regist_btn_ctrl.js
@@ -9,32 +9,19 @@ document.addEventListener('DOMContentLoaded', () => {
     return regex.test(password);
   }
 
-  function showTooltipMeil(element, message) {
-    const tooltip = document.getElementById("error-meil-msg");
-    const rect = element.getBoundingClientRect();
-    tooltip.textContent = message;
-    tooltip.style.display = "block";
-    tooltip.style.left = `${rect.left + window.scrollX + 110}px`;
-    tooltip.style.top = `${rect.bottom + window.scrollY + -65}px`;
-  }
-
   // ツールチップを表示する関数
-  function showTooltip(element, message) {
-    const tooltip = document.getElementById("error-msg");
-    const rect = element.getBoundingClientRect();
-    tooltip.textContent = message;
-    tooltip.style.display = "block";
-    tooltip.style.left = `${rect.left + window.scrollX + 170}px`;
-    tooltip.style.top = `${rect.bottom + window.scrollY + -65}px`;
+  function showTooltip(passElement, msg, tooltipId, xPos) {
+    const tooltipElement = document.getElementById(tooltipId);
+    const rect = passElement.getBoundingClientRect();
+    tooltipElement.textContent = msg;
+    tooltipElement.style.display = "block";
+    tooltipElement.style.left = `${rect.left + window.scrollX + xPos}px`;
+    tooltipElement.style.top = `${rect.bottom + window.scrollY + -65}px`;
   }
 
-  function hidePassTooltip() {
-    const tooltip = document.getElementById("error-meil-msg");
-    tooltip.style.display = "none";
-  }
-
-  function hideRePassTooltip() {
-    const tooltip = document.getElementById("error-msg");
+  // バリデーションチェックを通過した場合ツールチップを非表示にする関数
+  function hideTooltip(tooltipId) {
+    const tooltip = document.getElementById(tooltipId);
     tooltip.style.display = "none";
   }
 
@@ -42,13 +29,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // メールアドレスを取得
     const email = document.getElementById("email").value.trim();
     // パスワードを取得
-    const password = document.getElementById("password").value.trim();
+    const password = document.getElementById("password");
     // 再パスワードを取得
-    const rePassword = document.getElementById("re-password").value.trim();
+    const rePassword = document.getElementById("re-password");
     // 「同意する」のチェックボックスを取得
     const agreeChecked = document.getElementById("agreement").checked;
     // 送信ボタンを取得
     const submitBtn = document.getElementById("btn-regist");
+
+    const passwordVal = password.value.trim();
+    const rePasswordVal = rePassword.value.trim();
 
     // disabled判定用定数
     let isValid = true;
@@ -59,26 +49,35 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // メールアドレス、パスワード、再パスワードが入力されているか確認
-    if (email === '' || password === '' || rePassword === '') {
+    if (email === '' || passwordVal === '' || rePasswordVal === '') {
       isValid = false;
     }
 
     // パスワードの形式を確認
-    if (!validatePassword(password)) {
-      showTooltipMeil(document.getElementById("password"), "※8文字以上20文字以内の半角英数字で入力してください");
+    if (!validatePassword(passwordVal)) {
+      showTooltip(
+        password,
+        "※8文字以上20文字以内の半角英数字で入力してください",
+        "error-pass-msg",
+        110);
       isValid = false;
 
     } else {
       // エラーメッセージをクリア
-      hidePassTooltip();
+      hideTooltip("error-pass-msg");
     }
 
-    if (password !== rePassword) {
-      showTooltip(document.getElementById("re-password"), "※パスワードが一致しません");
+    if (passwordVal !== rePasswordVal) {
+      showTooltip(
+        rePassword,
+        "※パスワードが一致しません",
+        "error-repass-msg",
+        170
+      );
       isValid = false;
     } else {
       // エラーメッセージをクリア
-      hideRePassTooltip();
+      hideTooltip("error-repass-msg");
     }
 
     // ボタンを有効化
